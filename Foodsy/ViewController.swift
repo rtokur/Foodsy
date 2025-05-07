238039e236f53b3800e1fe731ebf361dadb0753c
Rümeysa Tokur <rtokur11@gmail.com>
2025-05-07 14:24:16 +0300

    Initial Commit

---
+++ Foodsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foodsy
+//
+//  Created by Rumeysa Tokur on 7.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
